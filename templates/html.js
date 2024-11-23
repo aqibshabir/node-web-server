d5f98ce9cbd61e3283b97f65d6c38aae93a8cec7
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-11-23 14:52:47 +0000

    templates directory - returning valid html using setHeader()

--- templates/html.js
+++ templates/html.js
@@ -4,7 +4,10 @@ const host = 'localhost';
 const port = 8000;
 
 const handleRequest = function (req, res) {
-  //
+  // content-type set to html
+  res.setHeader('Content-Type', 'text/html');
+  res.writeHead(200);
+  res.end('<html><body><h1>This is HTML</h1></body></html>');
 };
 
 const server = http.createServer(handleRequest);
