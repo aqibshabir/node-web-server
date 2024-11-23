5660a65845d43de6ae83468903e1de80cb7dcc39
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-11-23 19:28:04 +0000

    templates/html directory - serving html/css from file (caching files making it more efficient in some cases)

--- templates/html/html.js
+++ templates/html/html.js
@@ -10,37 +10,59 @@ const port = 8000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const htmlPath = join(__dirname, 'index.html'); // path for html
 const cssPath = join(__dirname, 'styles.css'); // path for css
-// can add other paths like JS file, Images, etc...
+// can add other paths like JS file, images, etc...
 
-const handleRequest = async (req, res) => {
-  let filePath, contentType;
-  if (req.url === '/') {
-    // on initial load - needs html content - so do this...
-    filePath = htmlPath;
-    contentType = 'text/html';
-  } else if (req.url === '/styles.css') {
-    // can see the link tag in html - browser automatically sends second request - so do this...
-    filePath = cssPath;
-    contentType = 'text/css';
-  } else {
-    // if can't find either - do this...
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Content not found');
-    return;
-  }
+// caching results before starting server
+let preLoadedHTML, preLoadedCSS;
+const preLoadedContent = async () => {
   try {
-    const contents = await fs.readFile(filePath); // filePath changes dependent on html/css
-    res.setHeader('Content-Type', contentType); // contentType changes dependent on html/css
-    res.writeHead(200);
-    res.end(contents);
+    preLoadedHTML = {
+      filePath: await fs.readFile(htmlPath),
+      contentType: 'text/html',
+    };
+    preLoadedCSS = {
+      filePath: await fs.readFile(cssPath),
+      contentType: 'text/css',
+    };
   } catch (error) {
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end(`${error}`);
+    console.error(`Could not read file: ${error}`);
+    process.exit(1);
   }
 };
 
-const server = http.createServer(handleRequest);
+const handleRequest = (req, res) => {
+  try {
+    let file;
 
-server.listen(port, host, () => {
-  console.log(`server is running on http://${host}:${port}`);
+    if (req.url === '/') {
+      // on initial load - needs html content - so do this...
+      file = preLoadedHTML;
+    } else if (req.url === '/styles.css') {
+      // can see the link tag in html - browser automatically sends second request - so do this...
+      file = preLoadedCSS;
+    } else {
+      // if can't find either - do this...
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Content not found');
+      return;
+    }
+
+    const contents = file.filePath; // filePath changes dependent on html/css
+    res.setHeader('Content-Type', file.contentType); // contentType changes dependent on html/css
+    res.writeHead(200);
+    res.end(contents);
+  } catch (error) {
+    console.error(error);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal Server Error');
+  }
+};
+
+// loading content before starting server
+preLoadedContent().then(() => {
+  const server = http.createServer(handleRequest);
+
+  server.listen(port, host, () => {
+    console.log(`server is running on http://${host}:${port}`);
+  });
 });
