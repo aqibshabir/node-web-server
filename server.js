c01635c1195df05b10fea22051df18139d27ef97
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-11-23 14:14:39 +0000

    my first web server with nodeJS

---
+++ server.js
@@ -0,0 +1,15 @@
+import * as http from 'http';
+
+const host = 'localhost';
+const port = 8000;
+
+const handleRequest = function (req, res) {
+  res.writeHead(200);
+  res.end('My first server');
+};
+
+const server = http.createServer(handleRequest);
+
+server.listen(port, host, () => {
+  console.log(`server is running on http://${host}:${port}`);
+});
