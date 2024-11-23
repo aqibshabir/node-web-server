4ff6d5e7eaef5ae884e605ad95b83930036c87d4
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-11-23 14:48:32 +0000

    templates directory - returning csv file using setHeader() (content-type and content-disposition)

--- templates/csv.js
+++ templates/csv.js
@@ -4,7 +4,12 @@ const host = 'localhost';
 const port = 8000;
 
 const handleRequest = function (req, res) {
-  //
+  // sets content-type to be csv:
+  res.setHeader('Content-Type', 'text/csv');
+  // csv file is an attachment and has a file name of 'data.csv'
+  res.setHeader('Content-Disposition', 'attachment;filename=data.csv');
+  res.writeHead(200);
+  res.end(`id,name,age\n1,Aqib,31`);
 };
 
 const server = http.createServer(handleRequest);
